bd2dad8a0981c41f3b5941e394af45fcd5636506
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-04-06 16:28:52 -0300

    Cria records de Medico e Endereco

---
+++ src/main/java/med/vol/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,10 @@
+package med.vol.api.medico;
+
+import med.vol.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome,
+                                  String email,
+                                  String crm,
+                                  Especialidade especialidade,
+                                  DadosEndereco endereco) {
+}
