443b6c4999280bbae1a5073cae75fa2a54291888
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-05-03 18:49:21 -0300

    método listar implementado

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -3,13 +3,15 @@ package med.vol.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.vol.api.medico.DadosCadastroMedico;
+import med.vol.api.medico.DadosListagemMedico;
 import med.vol.api.medico.Medico;
 import med.vol.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,6 +24,11 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 
